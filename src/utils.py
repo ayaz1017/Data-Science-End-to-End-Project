0caa36ca8f187fd35e8f4bf1cbf43ccdb0b2b6ac
ayaz1017 <your-email@example.com>
2025-07-15 23:46:23 +0530

    Adding components all files

--- src/utils.py
+++ src/utils.py
@@ -18,4 +18,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = models[model_name]
+            parameters = param[model_name]
 
+            gs = GridSearchCV(model, parameters, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
